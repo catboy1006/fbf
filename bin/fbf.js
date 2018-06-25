445426e799c1143c8eb8abff030ee0b79f767c28
chenhui <1114545064@qq.com>
2018-06-25 15:48:50 +0800

    v1.0.0

--- bin/fbf.js
+++ bin/fbf.js
@@ -1,8 +1,103 @@
 #!/usr/bin/env node
 const program = require('commander');
+const fs = require('fs');
+const path = require('path');
+const createHTML = require('create-html');
+const sizeOf = require('image-size');
+
+//排序
+const sortByFileName = files =>  {
+  const reg = /[0-9]+/g;
+  return files.sort((a, b) => {
+    let imga = (a.match(reg) || []).slice(-1),
+      imgb = (b.match(reg) || []).slice(-1)
+    return imga - imgb
+  });
+}
+
+//删除.DS_Store
+function deleteDS(file) {
+  file.map((v, i) => {
+    if(v === '.DS_Store'){
+      fs.unlink('img/.DS_Store', err => {})
+    }
+  })
+}
+
+// 生成keyframe
+function toCss(dir, fileList){
+  let _css = '';
+  let start = 0;
+  const per = Math.floor(100/fileList.length);
+  fileList.map((path, i) => {
+    if(i === fileList.length - 1){
+      _css += `
+        ${start + i*per}%, 100% {
+          background:url(./${dir}/${path}) center center no-repeat;
+          background-size:100% auto;
+        }
+      `
+    }else{
+      _css += `
+        ${start + i*per}% {
+          background:url(./${dir}/${path}) center center no-repeat;
+          background-size:100% auto;
+        }
+      `
+    }
+  })
+  return _css;
+}
+
 program.version('1.0.0', '-v, --version')
-  .command('init <name>')
-  .action((name) => {
-    console.log(name);
+  .command('init <dir>')
+  .action((dir) => {
+
+    const imgPath = path.resolve(dir)
+
+    let imgSize = null;
+    fs.readdir(imgPath, (err, file) => {
+      const newFile = sortByFileName(file)
+      deleteDS(newFile)
+      imgSize = sizeOf(dir + '/' +file[0]);
+      let frameCss = toCss(dir, newFile)
+
+      //取第一个图片的尺寸作为框尺寸
+      let cssString = `
+      .fbf-animation{
+        width: ${imgSize.width}px;
+        height: ${imgSize.height}px;
+        margin:auto;
+        background-image: url(./${dir}/${file[0]});
+        background-size: ${imgSize.width}px ${imgSize.height}px;
+        background-repeat: no-repeat;
+        animation-name: keyframes-img;
+        animation-duration: 0.36s;
+        animation-delay: 0s;
+        animation-iteration-count: infinite;
+        animation-fill-mode: forwards;
+        animation-timing-function: steps(1);
+      }
+      @keyframes keyframes-img {
+        ${frameCss}
+      }
+    `
+      let css = `
+      <style>${cssString}</style>
+    `
+      //生成html
+      let html = createHTML({
+        title: '逐帧动画',
+        scriptAsync: true,
+        lang: 'en',
+        dir: 'rtl',
+        head: '<meta name="description" content="example">',
+        body: '<div class="fbf-animation"></div>' + css,
+        favicon: 'favicon.png'
+      })
+      fs.writeFile('fbf.html', html, function (err) {
+        if (err) console.log(err)
+      })
+    })
   });
 program.parse(process.argv);
